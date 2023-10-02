349caa64c23cdb1355ba8deedcc2f4c9424fc3b2
l-kaushhik <contact.lokeshkaushik@gmail.com>
2023-10-02 20:18:04 +0530

    Created create node function

--- main.c
+++ main.c
@@ -3,12 +3,22 @@
 
 typedef struct Node{
     int data;
+    int size;
     struct Node *next;
 } Node;
 
+Node * createNode(){
+    Node *n = (Node *)malloc(sizeof(Node));
+    n->data = 0;
+    n->size = -1;
+    n->next = NULL;
+
+    return n;
+}
 
 int main()
 {
-    
+    Node *n = createNode();
+    printf("%d",n->data);
     return 0;
 }
