9d7371ebda97236004ec233401e005b7d8c6c930
l-kaushhik <contact.lokeshkaushik@gmail.com>
2023-10-04 14:51:54 +0530

    added check_null for checking null pointers

--- main.c
+++ main.c
@@ -7,13 +7,45 @@ typedef struct Node{
     struct Node *next;
 } Node;
 
+void check_null(void *ptr){
+    if(NULL == ptr){
+        fprintf(stderr, "Memory can't be allocated\n");
+        exit(EXIT_FAILURE);
+    }
+}
+
 Node * createNode(){
     Node *n = (Node *)malloc(sizeof(Node));
+    check_null(n);
     n->data = 0;
-    n->size = -1;
+    n->size = -1;   // -1 means node is empty
     n->next = NULL;
 
     return n;
+}
+
+void insertAtFirst(Node * n, int value){
+    
+}
+
+void insertAtIndex(){
+    //
+}
+
+void insertAtLast(){
+    // 
+}
+
+int deleteAtFirst(){
+    //
+}
+
+int deleteAtIndex(){
+
+}
+
+int deleteAtLast(){
+
 }
 
 int main()
