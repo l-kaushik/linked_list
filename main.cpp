e907c92c7e2742668ec22c7e1d145b808a4c0bcf
l-kaushhik <contact.lokeshkaushik@gmail.com>
2023-10-08 13:22:18 +0530

    Defines node class

--- main.cpp
+++ main.cpp
@@ -2,8 +2,20 @@
 
 using namespace std;
 
+
+class Node{
+public:
+    int data;
+    Node* next;
+    Node(int val){
+        data = val;
+        next = nullptr;
+    }
+};
+
 int main()
 {
-    cout<<"Hello World";
+    Node a = 4;
+    cout<<a.data;
     return 0;
 }
