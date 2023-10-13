6759a51a2f44d8759df8f02261adcaceb3ac8db4
l-kaushhik <contact.lokeshkaushik@gmail.com>
2023-10-13 17:45:15 +0530

    added isSorted flag for optimization

--- bubbleSort.c
+++ bubbleSort.c
@@ -7,18 +7,21 @@ typedef struct Node{
 
 void bubbleSort(Node *a, int size){
     int temp;
-
+    int isSorted;
     for(int i = 0; i<size-1; i++){
         Node *nextPtr = a->next;
         Node *currentPtr = a;
+        isSorted = 1;
         while(nextPtr != NULL){
             if(currentPtr->data > nextPtr->data){
                 temp = nextPtr->data;
                 nextPtr->data = currentPtr->data;
                 currentPtr->data = temp;
+                isSorted = 0;
             }
             currentPtr = nextPtr;
             nextPtr = nextPtr->next;
         }
+        if(isSorted) return;
     }
 }
