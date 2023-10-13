7aed2239f931cecc49bfb60d9a9c4fee4e5b37d5
l-kaushhik <contact.lokeshkaushik@gmail.com>
2023-10-13 17:51:00 +0530

    change the iterating condition to least possible no.

--- bubbleSort.c
+++ bubbleSort.c
@@ -8,11 +8,11 @@ typedef struct Node{
 void bubbleSort(Node *a, int size){
     int temp;
     int isSorted;
-    for(int i = 0; i<size-1; i++){
+    for(int i = 0; i<size-1; i++){  // loop for each pass
         Node *nextPtr = a->next;
         Node *currentPtr = a;
         isSorted = 1;
-        while(nextPtr != NULL){
+        for(int j = 0; j < size-i-1; j++){  // loop for lowest possible no. of comparisons in each pass
             if(currentPtr->data > nextPtr->data){
                 temp = nextPtr->data;
                 nextPtr->data = currentPtr->data;
@@ -22,6 +22,6 @@ void bubbleSort(Node *a, int size){
             currentPtr = nextPtr;
             nextPtr = nextPtr->next;
         }
-        if(isSorted) return;
+        if(isSorted) return;    // sorted flag for O(n) time complexity
     }
 }
