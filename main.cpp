1eaabde1582163dd142384bf7640f20b9b79ecf8
l-kaushhik <contact.lokeshkaushik@gmail.com>
2023-10-15 13:04:30 +0530

    Better structure

--- main.cpp
+++ main.cpp
@@ -1,178 +1,241 @@
-#include<iostream>
+#include <iostream>
 
 using namespace std;
 
-class Node{
+class Node
+{
 private:
     int data;
-    Node* next;
+    Node *next;
+
 public:
-    Node(int value, Node* next = nullptr){
+    Node(int value, Node *next = nullptr)
+    {
         this->data = value;
         this->next = next;
     }
 
-    int getValue(){return this->data;}
-    Node *getNext(){return this->next;}
-    void setNext(Node *next){this->next = next;}
+    int getValue() { return this->data; }
+    Node *getNext() { return this->next; }
+    void setNext(Node *next) { this->next = next; }
 };
 
-class LinkedList{
+class LinkedList
+{
 private:
     Node *head;
-   
-    // Insertion 
-    void __insert_at_beginning__(int value){
-        Node *newNode = new Node(value, head);
-        head = newNode;
+
+    void __insert_at_beginning__(int);
+    void __insert_at_index__(int , int );
+    void __insert_at_last__(int );
+    friend istream &operator>>(istream &, LinkedList &);
+    bool __check_null__(void *);
+    bool __remove_from_beginning__(int *);
+    bool __remove_from_particular_index__(int, int*);
+    bool __remove_from_end__(int *);
+    friend ostream &operator<<(ostream &os, const LinkedList &list);
+
+public:
+    LinkedList() { head = nullptr; }
+
+    // INSERTION
+    void prepend(int value) { __insert_at_beginning__(value); }
+    void insert(int index, int value) { __insert_at_index__(index, value); }
+    void append(int value) { __insert_at_last__(value); }
+
+    // DELETION
+    bool removeFirst(int *removedValue = nullptr) { return __remove_from_beginning__(removedValue); }
+    bool removeAt(int index, int *removedValue = nullptr) { return __remove_from_particular_index__(index, removedValue); }
+    bool removeLast(int *removedValue = nullptr) { return __remove_from_end__(removedValue); }
+};
+
+// <---------------- LINKED LIST FUNCTIONS DEFINITION ---------------->
+
+// Insertion
+void LinkedList::__insert_at_beginning__(int value)
+{
+    Node *newNode = new Node(value, head);
+    head = newNode;
+}
+
+void LinkedList::__insert_at_index__(int index, int value)
+{
+    try
+    {
+        if (index < 0)
+            throw invalid_argument("Index cannot be negative.");
+    }
+    catch (const exception &e)
+    {
+        cerr << "Error: " << e.what() << endl;
+        exit(EXIT_FAILURE);
     }
 
-    void __insert_at_index__(int index, int value){
-        try{
-            if(index < 0) throw invalid_argument("Index cannot be negative.");
-        } catch (const exception& e){
-            cerr<<"Error: "<<e.what()<<endl;
-            exit(EXIT_FAILURE);
-        }
-
-        if(index == 0) __insert_at_beginning__(value);
-        else{
-            int counter = 0;
-            Node *current = head, *temp;
-            while(counter != index-1){
-                try{
-                    if(current->getNext() == nullptr) throw out_of_range("List is out of range");
-                }catch(const exception& e){
-                    cerr<<"Error: "<<e.what()<<endl;
-                    exit(EXIT_FAILURE);
-                }
-                current = current->getNext();
-                counter++;
+    if (index == 0)
+        __insert_at_beginning__(value);
+    else
+    {
+        int counter = 0;
+        Node *current = head, *temp;
+        while (counter != index - 1)
+        {
+            try
+            {
+                if (current->getNext() == nullptr)
+                    throw out_of_range("List is out of range");
             }
-            temp = current->getNext();
-            current->setNext(new Node(value,temp));
-        }
-    }
-
-    void __insert_at_last__(int value){
-        if(head == nullptr){
-            head = new Node(value, nullptr);
-        }
-        else{
-            Node *current = head;
-            while(current->getNext() != nullptr){
-                current = current->getNext();
-            }
-            current->setNext(new Node(value));
-        }
-    }
-
-    friend istream& operator>>(istream& is, LinkedList& list){
-        int value;
-        is>>value;
-        list.__insert_at_last__(value);
-        return is;
-    }
-
-    // Deletion
-    // if removedValue pointer is given the removedValue will be given to it
-
-    bool __check_null__(void *ptr){
-        if(ptr == nullptr){return true;}
-        return false;
-    }
-
-    bool __remove_from_beginning__(int *removedValue){
-        if(__check_null__(head)){
-            cerr<<"List doesn't exists"<<endl;
-            return false;
-        }
-
-        Node * _mustFree = head;
-        int value = _mustFree->getValue();
-        head = head->getNext();
-        delete _mustFree;
-        if(!__check_null__(removedValue)) *removedValue = value;
-        return true;
-    }
-
-    bool __remove_from_particular_index__(int index, int *removedValue){
-        // if index is 0
-        if(index == 0)return __remove_from_beginning__(removedValue);
-
-        if(__check_null__(head)){
-            cerr<<"List doesn't exists"<<endl;
-            return false;
-        }
-
-        // index > 0 
-
-        int counter = 0, value;
-        Node * current = head, *temp = nullptr;
-        while(counter != index-1){
-            if(__check_null__(current)){
-                cerr<<"List out of range"<<endl;
-                return false;
+            catch (const exception &e)
+            {
+                cerr << "Error: " << e.what() << endl;
+                exit(EXIT_FAILURE);
             }
             current = current->getNext();
             counter++;
         }
         temp = current->getNext();
-        if(__check_null__(temp) || __check_null__(current)){
-            cerr<<"List out of range"<<endl;
-            return false;
+        current->setNext(new Node(value, temp));
+    }
+}
+
+void LinkedList::__insert_at_last__(int value)
+{
+    if (head == nullptr)
+    {
+        head = new Node(value, nullptr);
+    }
+    else
+    {
+        Node *current = head;
+        while (current->getNext() != nullptr)
+        {
+            current = current->getNext();
         }
-        current->setNext(temp->getNext());
-        value = temp->getValue();
-        if(!__check_null__(removedValue))*removedValue = value;
-        delete temp;
+        current->setNext(new Node(value));
+    }
+}
+
+istream &operator>>(istream &is, LinkedList &list)
+{
+    int value;
+    is >> value;
+    list.__insert_at_last__(value);
+    return is;
+}
+
+// Deletion
+// if removedValue pointer is given the removedValue will be given to it
+
+bool LinkedList::__check_null__(void *ptr)
+{
+    if (ptr == nullptr)
+    {
         return true;
     }
+    return false;
+}
 
-    bool __remove_from_end__(int* removedValue){
-        if(__check_null__(head)){
-            cerr<<"List doesn't exists"<<endl;
+bool LinkedList::__remove_from_beginning__(int *removedValue)
+{
+    if (__check_null__(head))
+    {
+        cerr << "List doesn't exists" << endl;
+        return false;
+    }
+
+    Node *_mustFree = head;
+    int value = _mustFree->getValue();
+    head = head->getNext();
+    delete _mustFree;
+    if (!__check_null__(removedValue))
+        *removedValue = value;
+    return true;
+}
+
+bool LinkedList::__remove_from_particular_index__(int index, int *removedValue)
+{
+    // if index is 0
+    if (index == 0)
+        return __remove_from_beginning__(removedValue);
+
+    if (__check_null__(head))
+    {
+        cerr << "List doesn't exists" << endl;
+        return false;
+    }
+
+    // index > 0
+
+    int counter = 0, value;
+    Node *current = head, *temp = nullptr;
+    while (counter != index - 1)
+    {
+        if (__check_null__(current))
+        {
+            cerr << "List out of range" << endl;
             return false;
         }
-        int value;
-        Node *current = head, *previous = nullptr;
-        while(current->getNext() != nullptr){
-            previous = current;
-            current = current->getNext();
-        }
-        value = current->getValue();
-        if(previous == nullptr){head = nullptr;}
-        else{previous->setNext(nullptr);}
-        delete current;
-        if(!__check_null__(removedValue))*removedValue = value;
-        return true;    
+        current = current->getNext();
+        counter++;
     }
-
-    // OUTPUT
-    friend ostream& operator<<(ostream& os, const LinkedList& list) {
-        Node *current = list.head;
-
-        while(current != nullptr){
-            os << current->getValue();
-            current = current->getNext();
-            if(current != nullptr) os <<" ";
-        }
-        return os;
+    temp = current->getNext();
+    if (__check_null__(temp) || __check_null__(current))
+    {
+        cerr << "List out of range" << endl;
+        return false;
     }
-public:
-    LinkedList(){head = nullptr;}
+    current->setNext(temp->getNext());
+    value = temp->getValue();
+    if (!__check_null__(removedValue))
+        *removedValue = value;
+    delete temp;
+    return true;
+}
 
-    // INSERTION 
-    void prepend(int value){__insert_at_beginning__(value);}
-    void insert(int index , int value){__insert_at_index__(index, value);}
-    void append(int value){ __insert_at_last__(value);}
+bool LinkedList::__remove_from_end__(int *removedValue)
+{
+    if (__check_null__(head))
+    {
+        cerr << "List doesn't exists" << endl;
+        return false;
+    }
+    int value;
+    Node *current = head, *previous = nullptr;
+    while (current->getNext() != nullptr)
+    {
+        previous = current;
+        current = current->getNext();
+    }
+    value = current->getValue();
+    if (previous == nullptr)
+    {
+        head = nullptr;
+    }
+    else
+    {
+        previous->setNext(nullptr);
+    }
+    delete current;
+    if (!__check_null__(removedValue))
+        *removedValue = value;
+    return true;
+}
 
-    // DELETION
-    bool removeFirst(int *removedValue=nullptr){return __remove_from_beginning__(removedValue);}
-    bool removeAt(int index,int *removedValue=nullptr){return __remove_from_particular_index__(index, removedValue);}
-    bool removeLast(int *removedValue=nullptr){return __remove_from_end__(removedValue);}
-    
-};
+// OUTPUT
+ostream &operator<<(ostream &os, const LinkedList &list)
+{
+    Node *current = list.head;
+
+    while (current != nullptr)
+    {
+        os << current->getValue();
+        current = current->getNext();
+        if (current != nullptr)
+            os << " ";
+    }
+    return os;
+}
+
+// <---------------- ENDS HERE ---------------->
 
 int main()
 {
@@ -180,13 +243,13 @@ int main()
     list.append(4);
     list.append(2);
     list.prepend(3);
-    list.insert(2,99);
-    cout<<list<<endl;
-    int removed ;
+    list.insert(2, 99);
+    cout << list << endl;
+    int removed;
     list.removeAt(2);
     list.removeLast();
     list.removeFirst();
     // cout<<removed<<endl;
-    cout<<list<<endl;
+    cout << list << endl;
     return 0;
 }
