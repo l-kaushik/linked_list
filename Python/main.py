2eef4684cc93e168857d40e996831b9907b0d44d
l-kaushhik <contact.lokeshkaushik@gmail.com>
2023-10-23 19:07:39 +0530

    created basic Node and LinkedList type

---
+++ Python/main.py
@@ -0,0 +1,13 @@
+
+class Node:
+    def __init__(self, data = None) -> None:
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+
+if __name__ == "__main__":
+    l = LinkedList()
